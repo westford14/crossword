d51f8273b19fff43456fddc47887c932f007eafe
Jon Fosdick <jfosdick@appnexus.com>
2016-11-27 01:42:15 -0500

    Save files more better

--- getDailyCrossword.sh
+++ getDailyCrossword.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 FORMATTEDDATE=$(date '+%Y-%m-%d')
-URL="http://www.nytimes.com/svc/crosswords/v2/puzzle/daily-$FORMATTEDDATE.puz"
+FILENAME="daily-$FORMATTEDDATE.puz"
+URL="http://www.nytimes.com/svc/crosswords/v2/puzzle/$FILENAME"
 USERNAME="jonathanfosdick91@gmail.com"
 PASSWORD="COaxing1"
 AUTHURL="https://myaccount.nytimes.com/mobile/login/smart/index.html?EXIT_URI=http%3A%2F%2Fwww.nytimes.com%2Fcrosswords%2Findex.html%3Fpage%3Dhome&_r=0"
-OUTPUTFILE="tmp/daily.puz"
+DIRECTORY="dailyCrosswords"
 
-mkdir tmp
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir $DIRECTORY
+fi
 
 #auth
 curl -b cookies -c cookies --data "username=$USERNAME&password=$PASSWORD&remember_me=1" $AUTHURL
@@ -15,5 +18,5 @@ curl -b cookies -c cookies --data "username=$USERNAME&password=$PASSWORD&remembe
 sleep 5
 
 #get today's crossword using auth cookie
-curl -b cookies -c cookies "$URL" > "$OUTPUTFILE"
+curl -b cookies -c cookies "$URL" > "$DIRECTORY/$FILENAME"
 
